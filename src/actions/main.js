cf4fc0a4ab641e6ed187177344ed985f0b069191
TomonoriHakiai <chu_noge0822@yahoo.co.jp>
2019-03-16 23:55:41 +0900

    create Action

---
+++ src/actions/main.js
@@ -0,0 +1,10 @@
+const ADD_MEMBER = 'ADD_MEMBER';
+const ADD_TODO = 'ADD_TODO';
+
+export const addMember = () => ({
+  type: 'ADD_MEMBER',
+})
+
+export const addTodo = () => ({
+  type: 'ADD_TODO',
+})
